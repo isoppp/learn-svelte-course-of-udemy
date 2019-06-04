1d5ca461fec7637a09b02ba4a1c5ba4fc9d8d637
isoppp <prog@d2deck.com>
2019-06-04 19:26:36 +0800

    Add MeetupItem component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,40 @@
 <script>
   import Header from './UI/Header.svelte';
+  import MeetupItem from './Meetups/MeetupItem.svelte'
 
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'In this meetup, we will have some experts that teach you how to code!',
+      imageUrl: 'https://via.placeholder.com/150',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply some rounds!',
+      imageUrl: 'https://via.placeholder.com/150',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com'
+    }
+  ]
 </script>
 
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
+
 <Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem></MeetupItem>
+  {/each}
+</section>
+
